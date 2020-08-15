4d874db9291f9bdf081e4c491b6f545eca25d18a
Tekeh <te276@cam.ac.uk>
2020-08-15 14:16:47 +0100

    Added geometric brownian fully, plus the evidence calculation. Also added try/catch blocks for tthe cases where the MAP is particularly flat, or where the true minimum is not reached and thus the hessian is not surely pos-def.

--- fintekeh.jl
+++ fintekeh.jl
@@ -11,7 +11,7 @@ end
 struct GeometricModel
 end
 
-
+################ STOCHASTIC TRAJECTORY SIMULATION ################
 function generate_trajectory(x0, model::BrownianModel, drift, diffusion, tf, no_samples)
 	# Generates drift-diffusion trajectory
 	dim = length(x0)
@@ -23,7 +23,7 @@ function generate_trajectory(x0, model::BrownianModel, drift, diffusion, tf, no_
 
 end
 
-function generate_trajectory(x0, model::CauchyMode, loc, scale, tf, no_samples)
+function generate_trajectory(x0, model::CauchyModel, loc, scale, tf, no_samples)
 	## Generates discontinuous levy flights with step size chosen from
 	# Cauchy distribution
 	dim = length(x0)
@@ -42,34 +42,12 @@ function generate_trajectory(x0, model::GeometricModel, p_drift, vol, tf, no_sam
 	dt = tf/no_samples
 	rand_vals=  randn(no_samples, dim)
 	times = dt*[i for i=1:no_samples, j=1:dim] 
-	x = x0 * transpose( exp.( (p_drift - vol^2/2)*times + vol *cumsum(rand_vals, dims=1)) )
-	return x
+	x = x0 * transpose(exp.( (p_drift - vol^2/2)*times + vol *cumsum(rand_vals, dims=1)))
+	return reshape(x, (100,1))
 end
 
-function infer(xt, model, dt=1, hessian::Bool=false)
-	# Bayesian parameter inference on drift and diffusion parameters
-	## Max A Posterior (MAP) estimate
-	sol = optimize(p -> mlogp(xt, p, dt, model),[-Inf,0], [Inf,Inf], [-1.,1.], Fminbox( LBFGS() ); autodiff = :forward ) ## note: seems to predict 2*diff ??
-	if hessian == true
-		hess = ForwardDiff.hessian(p -> mlogp(xt, p, dt, model), sol.minimizer)
-		return sol, hess
-	end
-end
-
-function mlogp(xt, p, dt, model::CauchyModel)
-	# Calculates log posterior given a drift diffusion model
-	# Appropriate normalization given
-	# p = (drift, diffusion)
-	dim = size(xt)[1]-1
-	logp = 0
-	for i in 1:dim
-		dx = xt[i+1,:] - xt[i,:]
-		prob = (dx .- p[1]).^2 .+ (dt*p[2])^2 
-		logp += - log.(prob)[1] + log(dt*p[2]) - log(π) 
-	end
-	return -logp
-end
-
+######### LOG P FUNCTIONS ###########################
+#
 function mlogp(xt, p, dt, model::BrownianModel)
 	# Calculates log posterior given a drift diffusion model
 	# p = (drift, diffusion)
@@ -83,6 +61,45 @@ function mlogp(xt, p, dt, model::BrownianModel)
 	return -logp
 end
 
+function mlogp(xt, p, dt, model::CauchyModel)
+	# Calculates log posterior given a drift diffusion model
+	# Appropriate normalization included
+	# p = (loc, scale)
+	dim = size(xt)[1]-1
+	logp = 0
+	for i in 1:dim
+		dx = xt[i+1,:] - xt[i,:]
+		prob = (dx .- p[1]).^2 .+ (dt*p[2])^2 
+		logp += - log.(prob)[1] + log(dt*p[2]) - log(π) 
+	end
+	return -logp
+end
+
+function mlogp(xt, p, dt, model::GeometricModel)
+	# Calculates log posterior given a geometric diffusion model
+	# Appropriate normalization included
+	# p = [%_drift, %_volatility]
+	dim = size(xt)[1]-1
+	logp = 0
+	for i in 1:dim
+		logp += -(log.(xt[i+1]) .- log.(xt[i]) .- (p[1] - 0.5 * p[2]^2) * dt).^2 ./(2 * p[2]^2 * dt)  .- log.(xt[i+1]) ## Beware floating-point madness in divisions
+	end
+	logp += -(dim/2) * log.(2*π*dt*p[2]^2)
+	return -logp
+end
+
+############## INFERENCE AND EVIDENCE #########################
+#
+function infer(xt, model, dt=1, hessian::Bool=false)
+	# Bayesian parameter inference on drift and diffusion parameters
+	## Max A Posterior (MAP) estimate
+	sol = optimize(p -> mlogp(xt, p, dt, model),[-Inf,0], [Inf,Inf], [-1.,1.], Fminbox( LBFGS() ); autodiff = :forward ) ## note: seems to predict 2*diff ??
+	if hessian == true
+		hess = ForwardDiff.hessian(p -> mlogp(xt, p, dt, model), sol.minimizer)
+		return sol, hess
+	end
+end
+	
 function isamp_evidence(xt, model, p_inf, inf_hess=I, points = 1000)
 	## Calculate Bayesian model evidence 
 	## p(x_t| M) = ∫ p(x| θ, M) p(θ | M) dθ
@@ -90,8 +107,11 @@ function isamp_evidence(xt, model, p_inf, inf_hess=I, points = 1000)
 	## Calculated using importance sampling
 	
 	dim = size(p_inf)[1]
+
 	invsig = 0.5 *( transpose(inf_hess) + inf_hess)
-	dist = MvNormal(p_inf, inv(invsig) )
+	sig = inv(invsig)
+	sig = 0.5*(sig + transpose(sig))
+	dist = MvNormal(p_inf, sig )
 	rvals = rand(dist, points)
 	
 	evidence = 0
@@ -105,40 +125,71 @@ function isamp_evidence(xt, model, p_inf, inf_hess=I, points = 1000)
 	return evidence#, rvals
 end
 
-	
+
 function time_correlator(xt)
 	y = [mean(xt[1+j:end].*xt[1:end-j]) - mean(xt[1+j:end]) * mean(xt[1:end-j]) for j in 1:size(xt)[1]-1 ]
 end
 
-
 ## Test the functions
 x0 = [1]
-drift = 0.93#*collect(1:5)
+drift = 0.93
 diff = 0.50
 
-loc = 0.56
+loc = 0.09
 scale = 0.17
+
+p_drift = 0.41
+p_vol = 0.13
+
 tf = 10
 no_samples = 100
 
 dt = tf/no_samples
 
 
-xc=generate_trajectory(x0, CauchyMode(), loc, scale, tf, no_samples)
-xt=generate_trajectory(x0, BrownianModel(), drift, diff, tf, no_samples)
-xg=generate_trajectory(x0, GeometricModel(), drift, diff, tf, no_samples)
-data = xc
+xc=generate_trajectory(x0, CauchyModel(), loc, scale, tf, no_samples)
+xb=generate_trajectory(x0, BrownianModel(), drift, diff, tf, no_samples)
+xg=generate_trajectory(x0, GeometricModel(), p_drift, p_vol, tf, no_samples)
+data = xg
 
 sol_b, hess_b = infer(data, BrownianModel(), dt, true)
-bars_b = sqrt.(diag(inv(hess_b)))
-println("drift is $(sol_b.minimizer[1]) +/- $(bars_b[1]) and diff is $(sol_b.minimizer[2]) +/- $(bars_b[2])") 
+try
+	bars_b = sqrt.(diag(inv(hess_b)))
+	println("BROWNIAN:\t drift is $(sol_b.minimizer[1]) +/- $(bars_b[1]) and diff is $(sol_b.minimizer[2]) +/- $(bars_b[2])") 
+catch e
+	println("BROWNIAN curvature @ MAP estimate is very flat or negative...")
+	global hess_b = I
+	bars_b = ["nan", "nan"]
+	println("BROWNIAN:\t drift is $(sol_b.minimizer[1]) +/- $(bars_b[1]) and diff is $(sol_b.minimizer[2]) +/- $(bars_b[2])") 
+end
+
+#println("BROWNIAN:\t drift is $(sol_b.minimizer[1]) +/- $(bars_b[1]) and diff is $(sol_b.minimizer[2]) +/- $(bars_b[2])") 
 ##
 sol_c, hess_c = infer(data, CauchyModel(), dt, true)
-bars_c = sqrt.(diag(inv(hess_c)))
-println("loc is $(sol_c.minimizer[1]) +/- $(bars_c[1]) and scale is $(sol_c.minimizer[2]) +/- $(bars_c[2])") 
+try
+	bars_c = sqrt.(diag(inv(hess_c)))
+	println("CAUCHY:\tloc is $(sol_c.minimizer[1]) +/- $(bars_c[1]) and scale is $(sol_c.minimizer[2]) +/- $(bars_c[2])") 
+catch e
+	println("CAUCHY curvature @ MAP estimate is very flat or negative...")
+	global hess_c = I
+	bars_c = ["nan", "nan"]
+	println("CAUCHY:\tloc is $(sol_c.minimizer[1]) +/- $(bars_c[1]) and scale is $(sol_c.minimizer[2]) +/- $(bars_c[2])") 
+end
+##
+sol_g, hess_g = infer(data, GeometricModel(), dt, true)
+try
+	bars_g = sqrt.(diag(inv(hess_g)))
+	println("GEOMETRIC:\tp_drift is $(sol_g.minimizer[1]) +/- $(bars_g[1]) and p_vol is $(sol_g.minimizer[2]) +/- $(bars_g[2])") 
+catch e
+	println("GEOMETRIC curvature @ MAP estimate is very flat or negative...")
+	global hess_g = I
+	bars_g = ["nan", "nan"]
+	println("GEOMETRIC:\tp_drift is $(sol_g.minimizer[1]) +/- $(bars_g[1]) and p_vol is $(sol_g.minimizer[2]) +/- $(bars_g[2])") 
+end
 
 ## evidence_calc
 #
-e1 = isamp_evidence(data, BrownianModel(), sol_b.minimizer, hess_b)
-e2 = isamp_evidence(data, CauchyModel(), sol_c.minimizer, hess_c)
-print(e1, "\n", e2)
+eb = isamp_evidence(data, BrownianModel(), 	sol_b.minimizer, hess_b)
+ec = isamp_evidence(data, CauchyModel(), 	sol_c.minimizer, hess_c)
+eg = isamp_evidence(data, GeometricModel(), 	sol_g.minimizer, hess_g)
+print("EVIDENCE VALUES \n\nBROWNIAN:\t $(eb) \nCAUCHY:\t  $(ec)\nGEOMETRIC:\t $(eg)")
